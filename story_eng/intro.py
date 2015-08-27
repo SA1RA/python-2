8ef8a229a8af05fcaca06c3d70e5d1a7bd67349d
Samuel Lucas <smlucas13@gmail.com>
2015-08-27 17:08:53 -0400

    save

---
+++ story_eng/intro.py
@@ -0,0 +1,19 @@
+"""A new begining
+
+You spawn in a field.
+The sun light burns your eyes as they adjust to the light.
+The grass moves slightly in the wind.
+INPUT(observe your surroundings.) if no
+A dark shape swing in from your left there is no time to react.
+You feel a terrrible pain in your head as you topple to the ground.
+You should have observed your surroundings.
+ELSE
+As you observe your surroundings you notice a forest off to your left and thin trails of smoke rising into the sky.
+INPUT(travle to the forest or to the town) if forest
+KILL
+ELSE
+As you walk towards the forest you notice a slight breeze and birds singing in the trees.
+The graphics were unbaleivible.
+As you reach the smoke you realize that it origanated from the smoke stacks of many building you reached the town.
+
+"""
